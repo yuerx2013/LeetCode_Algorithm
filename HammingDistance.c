547b0d6aec095a06dd539201c47d7937b4f0f0b4
yuerx2013 <33113055+yuerx2013@users.noreply.github.com>
2017-11-17 15:42:02 +0800

    Create HammingDistance.c

---
+++ HammingDistance.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int hammingDistance(int x, int y) {
+    int i=0,ret=0;
+  
+    for(i=0;i<32;i++)
+    {
+        if((x%2) != (y%2))
+        {
+            ret++;  
+        }
+        x=x/2;
+        y=y/2;
+    } 
+    
+    return ret;
+}
+
+int main()
+{
+  hammingDistance(1,4);
+  return 0;
+}
