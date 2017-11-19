1d40cda465f6bbd7a379228181cadb0100c68a15
yuerx2013 <33113055+yuerx2013@users.noreply.github.com>
2017-11-19 15:53:28 +0800

    6

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
-/*将输入的整数倒转过来输出*/
+/***************************************************************
+ * 函数功能：将输入的整数倒转过来输出
+ *
+ * 分析：第一，区分正负数；
+ * 第二，对得到的数值进行int范围考虑，一般用变量来替代数值大的数进行比较； 
+ ***************************************************************/
 int reverse(int x) {
   int flag = x;
-	int negative = 0;
-	long long ret = 0;
+  int negative = 0;
+  long long ret = 0;
   /*输入的数为负数，则设置一个变量为标志位*/ 
-	if(flag < 0)
+  if(flag < 0)
   {
-		flag = -flag;
-		negative = 1;
-	}
-  while(flag){
-		ret = ret*10 + flag % 10;
-		flag = flag / 10;
-    /*针对int的范围进行考虑*/
-		if((ret <-2147483647 )|| (ret >2147483647))
-        return 0;
+      flag = -flag;
+      negative = 1;
+  }
+  while(flag)
+  {
+      ret = ret*10 + flag % 10;
+      flag = flag / 10;
+      /*针对int的范围进行考虑*/
+      if((ret <-2147483647 )|| (ret >2147483647))
+           return 0;
   } 
-	if(negative == 1)
-	{
-		ret = -ret;
-	}
+  if(negative == 1)
+  {
+      ret = -ret;
+  }
   return (int)ret;
 }
 
